e5c2d13573935dae40c1368cbe4234206a9a1290
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-09-20 15:14:35 +0800

    connected to backend

--- mern-exercise-tracker/src/components/CreateUser.js
+++ mern-exercise-tracker/src/components/CreateUser.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class CreateUser extends Component {
@@ -28,6 +28,7 @@ export default class CreateUser extends Component {
     console.log(user)
 
     axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
     
     this.setState({
       username: ''

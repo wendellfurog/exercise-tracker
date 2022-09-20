9d933e74479959e1279d5f50c30e31ff567965be
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-09-20 17:11:29 +0800

    Added Create and Edit Exercise List

--- mern-exercise-tracker/src/components/CreateUser.js
+++ mern-exercise-tracker/src/components/CreateUser.js
@@ -25,8 +25,6 @@ export default class CreateUser extends Component {
       username: this.state.username
     }
 
-    console.log(user)
-
     axios.post('http://localhost:5000/users/add', user)
       .then(res => console.log(res.data));
     
